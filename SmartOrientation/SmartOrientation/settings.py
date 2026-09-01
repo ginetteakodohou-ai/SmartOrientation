8721cae98fff6ccd34f2fade2b6e62e7d1505973
Ginette Akodohou <ginetteakodohou@gmail.com>
2026-09-01 14:40:12 +0200

    Ajout des applications et de templates

--- SmartOrientation/SmartOrientation/settings.py
+++ SmartOrientation/SmartOrientation/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'utilisateur',
+    'formations',
+    'orientations',
+    'bourse'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'SmartOrientation.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
