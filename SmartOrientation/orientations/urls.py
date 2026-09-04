5e3bc30c3a56ec3f614bfc29af6e7419ba9ddf51
adioonifade69-ctrl <adioonifade69@gmail.com>
2026-09-04 12:10:43 +0200

    Add initial implementation of orientation analysis feature
    - Created models for Universite, Etablissement, Matiere, Baccalaureat, Filiere, Coefficient, and CentreInteret.
    - Implemented Django admin registration for the new models.
    - Added views and templates for orientation analysis, including form handling and data rendering.
    - Updated settings to allow all hosts for development purposes.
    - Configured URL routing to include orientation-related paths.
    - Created initial database migrations for the new models.
    - Added frontend logic for dynamic form handling and result display.

---
+++ SmartOrientation/orientations/urls.py
@@ -0,0 +1,7 @@
+# orientation/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('analyse/', views.orientation_view, name='orientation_analyse'),
+]
--- SmartOrientation/orientations/views.py
+++ SmartOrientation/orientations/views.py
@@ -1,3 +1,48 @@
+import json
 from django.shortcuts import render
+from .models import Baccalaureat, Filiere
 
-# Create your views here.
+def orientation_view(request):
+    donnees_matieres = {}
+    donnees_filieres = {}
+
+    bacs = Baccalaureat.objects.all()
+
+    for bac in bacs:
+        donnees_matieres[bac.nom] = []
+        donnees_filieres[bac.nom] = []
+
+        coefficients = bac.coefficients.select_related("matiere")
+
+        for coef in coefficients:
+            donnees_matieres[bac.nom].append({
+                "id": coef.matiere.id,
+                "nom": coef.matiere.nom,
+                "code": f"note_{coef.matiere.id}",
+                "coefficient": coef.coefficient,
+            })
+
+        filieres = Filiere.objects.filter(
+            bac=bac
+        ).select_related(
+            "etablissement",
+            "etablissement__Universite"
+        )
+
+        for filiere in filieres:
+            donnees_filieres[bac.nom].append({
+                "id": filiere.id,
+                "nom": filiere.nom,
+                "niveau": filiere.niveau,
+                "etablissement": filiere.etablissement.nom,
+                "universite": filiere.etablissement.Universite.nom,
+                "bourse": filiere.bourse,
+                "secour": filiere.secour,
+                "deboucher": filiere.deboucher,
+            })
+
+    context = {
+    "donnees_matieres_json": donnees_matieres,
+    "donnees_filieres_json": donnees_filieres,}
+
+    return render(request, "orientations/orientation.html", context)
