120c655766d384cb999f28ca7b3a8094fa3d2d52
adioonifade69-ctrl <adioonifade69@gmail.com>
2026-09-04 17:05:02 +0200

    feat: Mise à jour du fichier .gitignore pour inclure db* et suppression de l'URL admin dans les routes

--- SmartOrientation/utilisateur/urls.py
+++ SmartOrientation/utilisateur/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/admin',admin.site.urls),
     path('',views.accueil, name='accueil' ),
     path('inscription/',views.inscription, name='inscription' ),
     path('connexion/', views.connexion, name='connexion'),
